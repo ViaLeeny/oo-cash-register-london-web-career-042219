155bb7365298890bcce1b26e599bc5fa9dc29d2e
Ayleen  Nazario <ayleen.nazario@gmail.com>
2019-04-28 23:44:17 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,29 @@
-
+class CashRegister 
+  
+  attr_accessor :discount, :total, :items
+  
+  def initialize (discount = 0 )
+    @total = 0 
+    @discount = discount
+    @items = []
+  end 
+  
+  def total 
+    @total
+  end 
+  
+  def add_item (title, price, quantity = 1 )
+    title = self.total += price * quantity
+    @items << title 
+  end 
+  
+  def apply_discount
+    title*discount
+  end 
+  
+  
+  def items
+    @items
+  end 
+  
+end
